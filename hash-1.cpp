d1618add1297fdae8764e1dbe5532fdd4016ad44
QMCCC_OMEN <2868916930@qq.com>
2026-07-26 12:19:14 +0800

    哈希表单次遍历

--- hash-1.cpp
+++ hash-1.cpp
@@ -1,8 +1,24 @@
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
+
+// 哈希表单次遍历
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
+        unordered_map<int, int> valIndex;
+        int len = nums.size();
+
+        for (int i = 0; i < len; i++) {
+            if (valIndex.count(target - nums[i])) {
+                result.push_back(i);
+                result.push_back(valIndex[target - nums[i]]);
+                return result;
+            }
+            else valIndex[nums[i]] = i;
+        }
+        return result;
     }
 };
