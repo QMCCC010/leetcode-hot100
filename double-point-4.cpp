95cd763e861f2ae62ebc3ffdab0cd76b89a1aa63
QMCCC_OMEN <2868916930@qq.com>
2026-08-04 17:12:48 +0800

    新增双指针算法，空间复杂度降为O(1)

--- double-point-4.cpp
+++ double-point-4.cpp
@@ -37,3 +37,30 @@ public:
         return water;
     }
 };
+
+
+// 动态双指针，空间复杂度O(1)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int water = 0;
+        int left = 0, right = height.size() - 1;
+        int leftMax = 0, rightMax = 0;
+
+
+        while (left <= right) {
+            leftMax = max(leftMax, height[left]);
+            rightMax = max(rightMax, height[right]);
+
+            if (leftMax <= rightMax) {
+                water += leftMax - height[left];
+                left++;
+            }
+            else {
+                water += rightMax - height[right];
+                right--;
+            }
+        }
+        return water;
+    }
+};
